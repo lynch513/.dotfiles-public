d8926afa33de1f4fd88fa1396c02fb5798361675
lynch513 <lynch513@yandex.ru>
2022-06-19 19:47:16 +0500

    Cleanup nvim/init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -15,6 +15,7 @@ end
 --------------------------------------------------------------------------------------------
 --| # Plugins
 --------------------------------------------------------------------------------------------
+
 local install_path = vim.fn.stdpath 'data' .. '/site/pack/packer/start/packer.nvim'
 
 if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
@@ -48,131 +49,130 @@ require('packer').startup(function()
   ----------------------------------------------------------------------------------------
   use { 'nvim-telescope/telescope.nvim',
   requires = { { 'nvim-lua/popup.nvim' }, { 'nvim-lua/plenary.nvim' } }
-}
-----------------------------------------------------------------------------------------
---| ### Icons
-----------------------------------------------------------------------------------------
-use 'kyazdani42/nvim-web-devicons'
-----------------------------------------------------------------------------------------
---| ### Цветовая схема
-----------------------------------------------------------------------------------------
--- use 'sainnhe/everforest'
--- use 'shaunsingh/solarized.nvim'
-use 'shaunsingh/nord.nvim'
-use 'shaunsingh/moonlight.nvim'
--- use 'shaunsingh/doom-vibrant.nvim'
-----------------------------------------------------------------------------------------
---| ### Информационная строка внизу
-----------------------------------------------------------------------------------------
-use { 'nvim-lualine/lualine.nvim',
-config = function()
-  require('lualine').setup {
-    options = {
-      theme = 'nord'
+  }
+  ----------------------------------------------------------------------------------------
+  --| ### Icons
+  ----------------------------------------------------------------------------------------
+  use 'kyazdani42/nvim-web-devicons'
+  ----------------------------------------------------------------------------------------
+  --| ### Цветовая схема
+  ----------------------------------------------------------------------------------------
+  -- use 'sainnhe/everforest'
+  -- use 'shaunsingh/solarized.nvim'
+  use 'shaunsingh/nord.nvim'
+  use 'shaunsingh/moonlight.nvim'
+  ----------------------------------------------------------------------------------------
+  --| ### Информационная строка внизу
+  ----------------------------------------------------------------------------------------
+  use { 'nvim-lualine/lualine.nvim',
+  config = function()
+    require('lualine').setup {
+      options = {
+        theme = 'nord'
+      }
     }
-  }
-end, }
-----------------------------------------------------------------------------------------
---| ### Табы вверху
-----------------------------------------------------------------------------------------
-use { 'akinsho/bufferline.nvim', requires = 'kyazdani42/nvim-web-devicons',
-config = function()
-  require("bufferline").setup{}
-end, }
-----------------------------------------------------------------------------------------
---| ### Файловый менеджер
-----------------------------------------------------------------------------------------
--- use { 'kyazdani42/nvim-tree.lua',
---     requires = 'kyazdani42/nvim-web-devicons',
---     config = function() require'nvim-tree'.setup {} end,
--- }
-----------------------------------------------------------------------------------------
---| ### Навигация внутри файла по классам и функциям
-----------------------------------------------------------------------------------------
--- use 'majutsushi/tagbar'
-----------------------------------------------------------------------------------------
---| ### Add indentation guides even on blank lines
-----------------------------------------------------------------------------------------
-use { 'lukas-reineke/indent-blankline.nvim',
-config = function()
-  require("indent_blankline").setup {
-    show_current_context = true,
-    show_current_context_start = true,
-    char = '|',
-    space_char = '⋅',
-    filetype_exclude = { 'help', 'packer' },
-    buftype_exclude = { 'terminal', 'nofile' },
-    char_highlight = 'LineNr',
-    show_trailing_blankline_indent = true,
-  }
-end, }
+  end, }
+  ----------------------------------------------------------------------------------------
+  --| ### Табы вверху
+  ----------------------------------------------------------------------------------------
+  use { 'akinsho/bufferline.nvim', requires = 'kyazdani42/nvim-web-devicons',
+  config = function()
+    require("bufferline").setup{}
+  end, }
+  ----------------------------------------------------------------------------------------
+  --| ### Файловый менеджер
+  ----------------------------------------------------------------------------------------
+  -- use { 'kyazdani42/nvim-tree.lua',
+  --     requires = 'kyazdani42/nvim-web-devicons',
+  --     config = function() require'nvim-tree'.setup {} end,
+  -- }
+  ----------------------------------------------------------------------------------------
+  --| ### Навигация внутри файла по классам и функциям
+  ----------------------------------------------------------------------------------------
+  -- use 'majutsushi/tagbar'
+  ----------------------------------------------------------------------------------------
+  --| ### Add indentation guides even on blank lines
+  ----------------------------------------------------------------------------------------
+  use { 'lukas-reineke/indent-blankline.nvim',
+  config = function()
+    require("indent_blankline").setup {
+      show_current_context = true,
+      show_current_context_start = true,
+      char = '|',
+      space_char = '⋅',
+      filetype_exclude = { 'help', 'packer' },
+      buftype_exclude = { 'terminal', 'nofile' },
+      char_highlight = 'LineNr',
+      show_trailing_blankline_indent = true,
+    }
+  end, }
 
-----------------------------------------------------------------------------------------
---| ### Add git related info in the sign columns and popups
-----------------------------------------------------------------------------------------
-use { 'lewis6991/gitsigns.nvim', requires = { 'nvim-lua/plenary.nvim' } }
+  ----------------------------------------------------------------------------------------
+  --| ### Add git related info in the sign columns and popups
+  ----------------------------------------------------------------------------------------
+  use { 'lewis6991/gitsigns.nvim', requires = { 'nvim-lua/plenary.nvim' } }
 
-----------------------------------------------------------------------------------------
---| ## LSP и автодополнялка
-----------------------------------------------------------------------------------------
+  ----------------------------------------------------------------------------------------
+  --| ## LSP и автодополнялка
+  ----------------------------------------------------------------------------------------
 
--- Highlight, edit, and navigate code using a fast incremental parsing library
-use 'nvim-treesitter/nvim-treesitter'
--- Collection of configurations for built-in LSP client
-use 'neovim/nvim-lspconfig'
-use 'williamboman/nvim-lsp-installer'
--- Автодополнялка
-use 'hrsh7th/nvim-cmp'
-use 'hrsh7th/cmp-nvim-lsp'
-use 'hrsh7th/cmp-buffer'
-use 'saadparwaiz1/cmp_luasnip'
---- Автодополнлялка к файловой системе
-use 'hrsh7th/cmp-path'
--- Snippets plugin
-use 'L3MON4D3/LuaSnip'
+  -- Highlight, edit, and navigate code using a fast incremental parsing library
+  use 'nvim-treesitter/nvim-treesitter'
+  -- Collection of configurations for built-in LSP client
+  use 'neovim/nvim-lspconfig'
+  use 'williamboman/nvim-lsp-installer'
+  -- Автодополнялка
+  use 'hrsh7th/nvim-cmp'
+  use 'hrsh7th/cmp-nvim-lsp'
+  use 'hrsh7th/cmp-buffer'
+  use 'saadparwaiz1/cmp_luasnip'
+  --- Автодополнлялка к файловой системе
+  use 'hrsh7th/cmp-path'
+  -- Snippets plugin
+  use 'L3MON4D3/LuaSnip'
 
-----------------------------------------------------------------------------------------
--- ## HTML и CSS
-----------------------------------------------------------------------------------------
+  ----------------------------------------------------------------------------------------
+  -- ## HTML и CSS
+  ----------------------------------------------------------------------------------------
 
--- Подсвечивает закрывающий и откры. тэг. Если, где-то что-то не закрыто, то не подсвечивает.
-use 'idanarye/breeze.vim'
--- Закрывает автоматом html и xml тэги. Пишешь <h1> и он автоматом закроется </h1>
-use 'alvan/vim-closetag'
--- Подсвечивает #ffffff
-use 'ap/vim-css-color'
+  -- Подсвечивает закрывающий и откры. тэг. Если, где-то что-то не закрыто, то не подсвечивает.
+  use 'idanarye/breeze.vim'
+  -- Закрывает автоматом html и xml тэги. Пишешь <h1> и он автоматом закроется </h1>
+  use 'alvan/vim-closetag'
+  -- Подсвечивает #ffffff
+  use 'ap/vim-css-color'
 
-----------------------------------------------------------------------------------------
--- ## РАЗНОЕ
-----------------------------------------------------------------------------------------
+  ----------------------------------------------------------------------------------------
+  -- ## РАЗНОЕ
+  ----------------------------------------------------------------------------------------
 
--- Даже если включена русская раскладка vim команды будут работать
-use 'powerman/vim-plugin-ruscmd'
--- 'Автоформатирование' кода для всех языков
-use 'Chiel92/vim-autoformat'
--- ]p - вставить на строку выше, [p - ниже
-use 'tpope/vim-unimpaired'
--- Переводчик рус - англ
-use 'skanehira/translate.vim'
---- popup окошки
-use 'nvim-lua/popup.nvim'
--- Обрамляет или снимает обрамление. Выдели слово, нажми S и набери <h1>
-use 'tpope/vim-surround'
--- Считает кол-во совпадений при поиске
-use 'google/vim-searchindex'
--- Может повторять через . vimsurround
-use 'tpope/vim-repeat'
--- Стартовая страница, если просто набрать vim в консоле
-use 'mhinz/vim-startify'
--- Комментирует по gc все, вне зависимости от языка программирования
--- use { 'numToStr/Comment.nvim',
--- config = function() require('Comment').setup() end, }
--- Обрамляет строку в теги по ctrl- y + ,
-use 'mattn/emmet-vim'
--- Закрывает автоматом скобки
-use 'cohama/lexima.vim'
--- Линтер, работает для всех языков
-use 'dense-analysis/ale'
+  -- Даже если включена русская раскладка vim команды будут работать
+  use 'powerman/vim-plugin-ruscmd'
+  -- 'Автоформатирование' кода для всех языков
+  use 'Chiel92/vim-autoformat'
+  -- ]p - вставить на строку выше, [p - ниже
+  use 'tpope/vim-unimpaired'
+  -- Переводчик рус - англ
+  use 'skanehira/translate.vim'
+  --- popup окошки
+  use 'nvim-lua/popup.nvim'
+  -- Обрамляет или снимает обрамление. Выдели слово, нажми S и набери <h1>
+  use 'tpope/vim-surround'
+  -- Считает кол-во совпадений при поиске
+  use 'google/vim-searchindex'
+  -- Может повторять через . vimsurround
+  use 'tpope/vim-repeat'
+  -- Стартовая страница, если просто набрать vim в консоле
+  use 'mhinz/vim-startify'
+  -- Комментирует по gc все, вне зависимости от языка программирования
+  -- use { 'numToStr/Comment.nvim',
+  -- config = function() require('Comment').setup() end, }
+  -- Обрамляет строку в теги по ctrl- y + ,
+  use 'mattn/emmet-vim'
+  -- Закрывает автоматом скобки
+  use 'cohama/lexima.vim'
+  -- Линтер, работает для всех языков
+  use 'dense-analysis/ale'
 end)
 
 --------------------------------------------------------------------------------------------
