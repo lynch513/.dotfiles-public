b27cfd31da3a8824d71dca5cd1849697d4777da4
lynch513 <lynch513@yandex.ru>
2022-06-24 00:55:18 +0500

    Changed .emacs.d/config.org

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -8,7 +8,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(evil-nerd-commenter vertico undo-fu peep-dired dired-open all-the-icons-dired dashboard all-the-icons gcmh doom-modeline evil-collection evil use-package)))
+   '(emojify marginalia evil-nerd-commenter vertico undo-fu peep-dired dired-open all-the-icons-dired dashboard all-the-icons gcmh doom-modeline evil-collection evil use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
